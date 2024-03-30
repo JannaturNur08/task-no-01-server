59f1bbd1fb34a6e7cc73bb35ce2148f3b8ad60ab
JannaturNur08 <nur@playense.com>
2024-03-30 11:17:34 +0600

    post api created for payment

--- index.js
+++ index.js
@@ -207,6 +207,23 @@ async function run() {
 			res.send(result);
 		});
 
+		 // payment intent
+		 app.post('/create-payment-intent', async (req, res) => {
+			const { price } = req.body;
+			const amount = parseInt(price * 100);
+			console.log(amount, 'amount inside the intent')
+	  
+			const paymentIntent = await stripe.paymentIntents.create({
+			  amount: amount,
+			  currency: 'usd',
+			  payment_method_types: ['card']
+			});
+	  
+			res.send({
+			  clientSecret: paymentIntent.client_secret
+			})
+		  });
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log(
