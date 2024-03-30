bbabf05ab1c840e0bca905b6208b79015e9a870c
JannaturNur08 <nur@playense.com>
2024-03-30 13:18:53 +0600

    get api created for admin statistics

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
@@ -209,22 +209,79 @@ async function run() {
 			res.send(result);
 		});
 
-		 // payment intent
-		 app.post('/create-payment-intent', async (req, res) => {
+		// payment intent
+		app.post("/create-payment-intent", async (req, res) => {
 			const { price } = req.body;
 			const amount = parseInt(price * 100);
-			console.log(amount, 'amount inside the intent')
-	  
+			console.log(amount, "amount inside the intent");
+
 			const paymentIntent = await stripe.paymentIntents.create({
-			  amount: amount,
-			  currency: 'usd',
-			  payment_method_types: ['card']
+				amount: amount,
+				currency: "usd",
+				payment_method_types: ["card"],
 			});
+
+			res.send({
+				clientSecret: paymentIntent.client_secret,
+			});
+		});
+
+		app.get("/payments/:email", verifyToken, async (req, res) => {
+			const query = { email: req.params.email };
+			if (req.params.email !== req.decoded.email) {
+				return res.status(403).send({ message: "forbidden access" });
+			}
+			const result = await paymentCollection.find(query).toArray();
+			res.send(result);
+		});
+
+		app.post("/payments", async (req, res) => {
+			const payment = req.body;
+			const paymentResult = await paymentCollection.insertOne(payment);
+
+			//  carefully delete each item from the cart
+			console.log("payment info", payment);
+			const query = {
+				_id: {
+					$in: payment.cartIds.map((id) => new ObjectId(id)),
+				},
+			};
+
+			const deleteResult = await cartCollection.deleteMany(query);
+
+			res.send({ paymentResult, deleteResult });
+		});
+
+		 // stats or analytics
+		 app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
+			const users = await userCollection.estimatedDocumentCount();
+			const menuItems = await menuCollection.estimatedDocumentCount();
+			const orders = await paymentCollection.estimatedDocumentCount();
+	  
+			// this is not the best way
+			// const payments = await paymentCollection.find().toArray();
+			// const revenue = payments.reduce((total, payment) => total + payment.price, 0);
+	  
+			const result = await paymentCollection.aggregate([
+			  {
+				$group: {
+				  _id: null,
+				  totalRevenue: {
+					$sum: '$price'
+				  }
+				}
+			  }
+			]).toArray();
+	  
+			const revenue = result.length > 0 ? result[0].totalRevenue : 0;
 	  
 			res.send({
-			  clientSecret: paymentIntent.client_secret
+			  users,
+			  menuItems,
+			  orders,
+			  revenue
 			})
-		  });
+		  })
 
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
